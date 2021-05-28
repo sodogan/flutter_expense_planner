7307be65aa70c39a1d85fa3add81531361966b7a
sodogan <itelligence.i.dogan@daimler.com>
2021-05-28 22:34:51 +0300

    minor

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './transaction.dart' show Transaction;
 
 void main() => runApp(MyApp());
 
@@ -13,14 +14,41 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction.now(id: "t1", title: "New Shoes", amount: 69.99),
+    Transaction.now(id: "t2", title: "New Book", amount: 19.99),
+    Transaction.now(id: "t3", title: "New Bicycle", amount: 420.99),
+    Transaction.now(id: "t4", title: "New Shampoo", amount: 29.99),
+  ];
+
+  List<Widget> mappedList() {
+    var result = transactions.map((e) => Card(child: Text(e.title))).toList();
+    return result;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter App'),
       ),
-      body: Center(
-        child: Text('Widget Playground!'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            child: Card(
+              child: Text("Chart"),
+              color: Colors.blue,
+              elevation: 12,
+            ),
+            width: double.infinity,
+            height: 35,
+          ),
+          Column(children: [
+            ...mappedList(),
+          ]),
+        ],
       ),
     );
   }
